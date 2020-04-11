8d5651f3bc65cc5e321829731d63e5b378314374
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-04-11 16:57:45 -0500

    little changes

--- DequeExample.java
+++ DequeExample.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 class DequeExample {
 	public static void main(String [] args){
+		Deque<String> d = new LinkedList<String>();
 
+		d.add("5");
+		d.addFirst("1");
+		d.addLast("");
 	}
 }
